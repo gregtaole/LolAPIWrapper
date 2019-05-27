2c14e40d7f3721a4eb82be9577932fb9d0b3622c
Gregory LE <contact@gregtaole.fr>
2019-05-27 22:00:54 +0200

    Deleted go struct files

--- test_data/summoner/by_account/2DZY2QJXEvm1zUVmC-dyE87EKcfT2n56thH4DGHnVKCpUTw.go
+++
@@ -1,9 +0,0 @@
-SummonerDTO{
-    Name: Dinervoid,
-    ID: eolRXteXeqUQpy6lVgGSGYMGs7ht9cWFkvAlNUtOQ_01UzQ,
-    AccountID: 2DZY2QJXEvm1zUVmC-dyE87EKcfT2n56thH4DGHnVKCpUTw,
-    Puuid: XHmNrfsNaJ0rRpCU6zVQakbpunyetW-zCGwEomsm1_0-XN4vtj4agwEiZbLeAQBuKURspO7Z2ThVrw,
-    SummonerLevel: 57,
-    ProfileIconID: 3015,
-    RevisionDate: 1550101518000,
-}
--- test_data/summoner/by_id/_JEwwuOSAIZuxsZYp1V3mObKjIWjle8eAZgXWfoksGns74U.go
+++
@@ -1,9 +0,0 @@
-SummonerDTO{
-    Name: Mikrogeo,
-    ID: _JEwwuOSAIZuxsZYp1V3mObKjIWjle8eAZgXWfoksGns74U,
-    AccountID: Yt_Pv9-mJlDI8j3j8vvDs0MOcKSkpFvhs3Gca8O9iOSWE9s,
-    Puuid: KRfzFRAmuCjxd7GsylVVLZdWd6X15IREkxcDKR-nC5UtPWo6KnH5mcLFm5Ujk3CwYvIYtjiLnB9jLA,
-    SummonerLevel: 115,
-    ProfileIconID: 4022,
-    RevisionDate: 1558301354000,
-}
